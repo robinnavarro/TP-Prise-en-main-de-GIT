350155c2b02a0342226bceb1357178cd42fa3234
Robin Navarro <rnavarro@expertime.com>
2026-07-23 15:18:14 +0200

    New text button

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App: React.FC = () => {
                 className={styles.button}
                 onClick={() => window.open('https://aka.ms/codeapps', '_blank')}
               >
-                Let's Go
+                Démo 2626
               </Button>
             </div>
           </Card>
