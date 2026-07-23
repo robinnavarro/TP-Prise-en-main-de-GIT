5e306ae4fac8e90f903a5f4457f3b024922c487e
Robin Navarro <rnavarro@expertime.com>
2026-07-23 15:15:07 +0200

    Update button text to 'PowerAppsCodeApps Repo!!!'

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App: React.FC = () => {
                 className={styles.button}
                 onClick={() => window.open('https://aka.ms/codeapps', '_blank')}
               >
-                Let's Go
+               PowerAppsCodeApps Repo!!!
               </Button>
             </div>
           </Card>
